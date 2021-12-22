8b46801a7970292ba53f05ff8767fb8d7e54bc4b
Omarhuba <90012112+Omarhuba@users.noreply.github.com>
2021-12-22 22:21:42 +0100

    fix responsive

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import "./styles/main.scss";
 
+ducument.getElementById("login").addEventListener("click", ()=>{
+    console.log('login my page!')
+})
+
+const btn = document.querySelector('button');
+btn.addEventListener("click", ()=>{
+    alert('Hello to my new webpack!')
+})
+
+
 console.log("Hello World!");
