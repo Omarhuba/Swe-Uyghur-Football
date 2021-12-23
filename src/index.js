9fb3ff09a7ec3b7f9da52d62723592433568919c
Omarhuba <90012112+Omarhuba@users.noreply.github.com>
2021-12-23 21:26:34 +0100

    add dropdown

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import "./styles/main.scss";
 
-ducument.getElementById("login").addEventListener("click", ()=>{
+ducument.getElementById("login").addEventListener("click", () => {
     console.log('login my page!')
 })
 
+
 const btn = document.querySelector('button');
-btn.addEventListener("click", ()=>{
+btn.addEventListener("click", () => {
     alert('Hello to my new webpack!')
 })
+
+// dropdown function
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+// }
+// myFunction()
+// window.onclick = function (event) {
+//     if (!event.target.matchs('.menuBtn')) {
+//         let dropdowns = document.getElementById("dropdown-content")
+//         // let i;
+//         for (let i = 0; i < dropdowns.length; i++) {
+//             let openDropdown = dropdowns[i];
+//             if (openDropdown.cliassList.contains('show')) {
+//                 openDropdown.classList.remove('show')
+//             }
+//         }
+//     }
+// }
+
 
 
 console.log("Hello World!");
